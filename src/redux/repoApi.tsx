ac86f2b89efb0435e1c95a3ecf2a61e10c42c0b8
Xuan <97ssps30212@gmail.com>
2022-02-15 21:05:35 +0800

    Cache data for 5 minutes

--- src/redux/repoApi.tsx
+++ src/redux/repoApi.tsx
@@ -3,15 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import RepoInfo from 'types/RepoInfo';
 import FileInfo from 'types/FileInfo';
 
+const cachedDuration = 300; // 5 minutes
+
 export const repoApi = createApi({
   reducerPath: 'repoApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com/' }),
   endpoints: builder => ({
     getRepo: builder.query<RepoInfo, { username: string; repo: string }>({
       query: info => `repos/${info.username}/${info.repo}`,
+      keepUnusedDataFor: cachedDuration,
     }),
     getFiles: builder.query<FileInfo[], { username: string; repo: string }>({
       query: info => `repos/${info.username}/${info.repo}/contents`,
+      keepUnusedDataFor: cachedDuration,
     }),
   }),
 });

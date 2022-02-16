6ad5838309fd39b4a20e26ef0232293a27532a90
Xuan <97ssps30212@gmail.com>
2022-02-16 13:15:07 +0800

    Move Modal component to App component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,20 +3,17 @@ import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import withAnimation from 'hocs/withAnimation';
-import { useAppDispatch, useAppSelector } from 'redux/store';
+import { useAppDispatch } from 'redux/store';
 import { reset } from 'redux/repoListSlice';
-import { show, hide, setMessage } from 'redux/modalSlice';
+import { show, setMessage } from 'redux/modalSlice';
 import { fetchRepos } from 'githubApi';
 import { Container, Header, StyledSvg } from './Home.style';
 import Form from 'components/Form';
-import Modal from 'components/Modal';
-import Dialog from 'components/Dialog';
 
 const Home = () => {
   const navigate = useNavigate();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const appDispatch = useAppDispatch();
-  const modal = useAppSelector(state => state.modal);
   const onFormSubmit = useCallback(
     async (username: string) => {
       setIsSubmitting(true);
@@ -42,19 +39,14 @@ const Home = () => {
   );
 
   return (
-    <>
-      <Modal show={modal.show}>
-        <Dialog onClick={() => appDispatch(hide())} message={modal.message} />
-      </Modal>
-      <Container>
-        <Header>
-          <StyledSvg href="icon-github"></StyledSvg>
-          <h1>Github Repositories</h1>
-        </Header>
+    <Container>
+      <Header>
+        <StyledSvg href="icon-github"></StyledSvg>
+        <h1>Github Repositories</h1>
+      </Header>
 
-        <Form isSubmitting={isSubmitting} onFormSubmit={onFormSubmit} />
-      </Container>
-    </>
+      <Form isSubmitting={isSubmitting} onFormSubmit={onFormSubmit} />
+    </Container>
   );
 };
 

03e434eb5e48534e8e99eb278c65a9fa96294e15
Xuan <97ssps30212@gmail.com>
2022-04-14 22:31:07 +0800

    Rename githubApi.tsx to githubApi.ts

--- src/githubApi.ts
+++ src/githubApi.ts
@@ -1,19 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 
-import RepoInfo from 'types/RepoInfo';
-
 const githubApi = axios.create({
   baseURL: 'https://api.github.com/',
   params: {
     per_page: 10,
   },
 });
-
-export const fetchRepos = (username: string, pageNumber = 1) => {
-  return githubApi.get<any, AxiosResponse<RepoInfo[]>>(`/users/${username}/repos`, {
-    params: { page: pageNumber },
-  });
-};
 
 export const searchUser = (username: string) => {
   return githubApi.get<any, AxiosResponse<{ items: { login: string }[] }>>('/search/users', {
